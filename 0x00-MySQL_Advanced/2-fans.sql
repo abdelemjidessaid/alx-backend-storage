c541a5ab97fe2751ae790fd6a649832f21128cd1
Abdelemjid Essaid <abdelemjidessaid@gmail.com>
2024-01-17 15:48:19 +0100

    4th task is done

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,7 +1,7 @@
 -- Script that summats the fans of origin
 -- Of origin and fans column
 
-SELECT origin, SUM(fans) AS nb_fans
+SELECT origin, SUM(fans) as nb_fans
 FROM metal_bands
 GROUP BY origin
 ORDER BY nb_fans DESC;
