c5c6db21b85725c7489652a5d858050a7f9bbcb1
Abdelemjid Essaid <abdelemjidessaid@gmail.com>
2024-01-24 15:33:15 +0100

    the advanced task is updated

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,64 +1,36 @@
 #!/usr/bin/env python3
 """
-    Module that contains a function that obtain the
-    HTML content of a particular URL and returns it
+web cache and tracker
 """
 import requests
 import redis
-import time
 from functools import wraps
 
+store = redis.Redis()
 
-r = redis.Redis(host='localhost', port=6379, db=0)
 
-
-def cache_page(func):
-    """
-        Function that caches the page content
-    """
-    @wraps(func)
+def count_url_access(method):
+    """ Decorator counting how many times
+    a URL is accessed """
+    @wraps(method)
     def wrapper(url):
-        """
-            Wrapper function
-        """
-        cached_result = r.get(url)
-        if cached_result is not None:
-            return cached_result.decode()
+        cached_key = "cached:" + url
+        cached_data = store.get(cached_key)
+        if cached_data:
+            return cached_data.decode("utf-8")
 
-        page_content = func(url)
-        r.setex(url, 10, page_content)
+        count_key = "count:" + url
+        html = method(url)
 
-        count_key = f"count:{url}"
-        r.incr(count_key)
-
-        return page_content
-
+        store.incr(count_key)
+        store.set(cached_key, html)
+        store.expire(cached_key, 10)
+        return html
     return wrapper
 
 
-@cache_page
-def get_page(url):
-    """
-        Function that fetches the HTML content of the visited URL
-    """
-    response = requests.get(url)
-    return response.text
-
-
-slow_url = "http://slowwly.robertomurray.co.uk"
-page_content = get_page(slow_url)
-print(page_content)
-
-cached_content = get_page(slow_url)
-print(cached_content)
-
-time.sleep(10)
-
-new_content = get_page(slow_url)
-print(new_content)
-
-count_key = f"count:{slow_url}"
-access_count = r.get(count_key)
-print(f"Access count for {slow_url}: {access_count.decode()}")
-
-r.close()
+@count_url_access
+def get_page(url: str) -> str:
+    """ Returns HTML content of a url """
+    res = requests.get(url)
+    return res.text
