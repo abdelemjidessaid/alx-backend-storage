1996c5456b8b048fabde083d1942988725c349f9
Abdelemjid Essaid <abdelemjidessaid@gmail.com>
2024-01-24 14:58:13 +0100

    2nd task is done

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,7 +4,7 @@
 """
 import redis
 import uuid
-from typing import Union
+from typing import Union, Optional, Callable, Any
 
 
 class Cache:
@@ -29,3 +29,31 @@ class Cache:
             return key
         else:
             return None
+
+    def get(self, key: str, fn: Optional[Callable] = None) -> Any:
+        """
+            Method that returns the data with the specific type
+        """
+        client = self._redis
+        value = client.get(key)
+        if not value:
+            return
+        if fn is int:
+            return self.get_int(value)
+        if fn is str:
+            return self.get_str(value)
+        if callable(fn):
+            return fn(value)
+        return value
+
+    def get_str(self, data: bytes) -> str:
+        """
+            Method that converts bytes to string
+        """
+        return data.decode('utf-8')
+
+    def get_int(self, data: bytes) -> int:
+        """
+            Method that converts bytes to integers
+        """
+        return int(data)
