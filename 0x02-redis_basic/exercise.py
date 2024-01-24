bf0c5845f36fd65ca44557fa618e9f1d6635aace
Abdelemjid Essaid <abdelemjidessaid@gmail.com>
2024-01-24 14:49:12 +0100

    1st task is updated

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -16,8 +16,8 @@ class Cache:
         """
             The class Cache Constructor
         """
-        self.__redis = redis.Redis()
-        self.__redis.flushdb()
+        self._redis = redis.Redis()
+        self._redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
@@ -25,7 +25,7 @@ class Cache:
         """
         if data:
             key = str(uuid.uuid4())
-            self.__redis.set(key, data)
+            self._redis.set(key, data)
             return key
         else:
             return None
